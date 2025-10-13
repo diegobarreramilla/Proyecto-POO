cd7595a6e6f7cae0016931418758dfa82e1bffb3
diegobarreramilla <diego.barrera.milla@gmail.com>
2025-10-12 18:15:10 -0700

    Este es mi primer commit

---
+++ Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+
+    public static void main(String[] args){
+        Principal p = new Principal();
+        p.menu();
+    }
+
+    public void menu(){
+
+    }
+
+    
+}
